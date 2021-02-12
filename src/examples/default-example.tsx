e91f4eb1c9209a3b162128c85a16a81d889b3596
Evgenii Khramkov <ewgeniux@gmail.com>
2021-02-12 17:19:59 +0300

    WIP

--- src/examples/default-example.tsx
+++ src/examples/default-example.tsx
@@ -10,7 +10,6 @@ import {
   PanelHeader,
   Group,
 } from "@vkontakte/vkui";
-import { SandboxEmbed } from "@codesandbox/react-embed";
 import "@vkontakte/vkui/dist/vkui.css";
 
 export function Example() {
@@ -20,24 +19,7 @@ export function Example() {
         <View activePanel="panel1">
           <Panel id="panel1">
             <PanelHeader>Panel</PanelHeader>
-            <Group>
-              <SandboxEmbed
-                sandboxOptions={{
-                  name: "VKUI - Basic Example",
-                  examplePath: "src/examples/default-example.tsx",
-                  gitInfo: {
-                    account: "ewgenius",
-                    repository: "vkui-examples",
-                    host: "github",
-                  },
-                }}
-                embedOptions={{
-                  codemirror: true,
-                  fontsize: 14,
-                }}
-                height="100vh"
-              ></SandboxEmbed>
-            </Group>
+            <Group></Group>
           </Panel>
         </View>
       </SplitCol>
